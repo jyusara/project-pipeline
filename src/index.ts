9739de298bb6ae892e156ba7a6a4f5906e7c43b1
Rodrigo Chumpitaz <98637169+RodrigoChumpitaz@users.noreply.github.com>
2025-02-19 19:37:49 -0500

    feat: Rodrigo Chumpitaz changes

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { seedAgents } from './seeders';
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-  // Rodrigo Chumpitaz: Change index.ts v2
+  // Rodrigo Chumpitaz: Change index.ts v3
   //await dbConnection.default();
   //await seedAgents();
 })();
